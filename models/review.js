f9615f4003fa7a260cb22f46595de788a485c2ce
Fran <fmjerez@icloud.com>
2020-02-17 21:42:38 +0100

    Search bar added

--- models/review.js
+++ models/review.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const strength = require('strength');
+
 
 const reviewSchema = new Schema(
   {
@@ -20,3 +22,6 @@ const reviewSchema = new Schema(
 
 const Review = mongoose.model("Review", reviewSchema);
 module.exports = Review;
+
+//User.find().populate("username") antes de llamarlo hay que configurarlo en la colección como en las líneas 6 y 7. Llamando a user con populate te devuelve lo que le hayas indicado 
+
