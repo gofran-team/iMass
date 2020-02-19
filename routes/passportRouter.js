a0a1bdd3a99e45bd0d5acc9a72944b5ee03d2aaa
Gonzalo Hernández <gonzalohernandez@icloud.com>
2020-02-19 21:39:52 +0100

    sass files, login after sign up and search route

--- routes/passportRouter.js
+++ routes/passportRouter.js
@@ -4,80 +4,55 @@ const User = require("../models/user");
 const bcrypt = require("bcrypt");
 const passport = require("passport");
 const { isLoggedIn, isLoggedOut } = require("../lib/isLoggedMiddleware");
-const strength = require('strength');
-const {hashPassword,checkHashed} = require("../lib/hash");
+const strength = require("strength");
+const { hashPassword, checkHashed } = require("../lib/hash");
+const axios = require("axios");
 
-
-
+// Sign up
 router.get("/signup", isLoggedOut(), (req, res) => {
   res.render("passport/signup", {
     user: req.user
   });
 });
-// Sign up
-router.post('/signup', isLoggedOut(), async (req, res, next) => {
-  const {
-    username,
-    password
-  } = req.body;
-  if (!username || !password) {
-    req.flash("error", "Please, fill all the fields");
-    return res.redirect("/signup");
-  }
-    try {
-      const existingUser = await User.findOne({
-        username
-      });
-      if (!existingUser && strength(password) >= 2) {
-        const newUser = await User.create({
-          username,
-          password: hashPassword(password)
-        });
-        return res.redirect('/login');
-      } else if (strength(password) < 2) {
-        req.flash("error", "Create a password with mixed case, special character and number (minimun 8 characters and no repeated letters)");
-        return res.redirect('/signup');
-      } else {
-        req.flash("error", "The user or password already exists");
-        return res.redirect('/signup')
-      }
-    } catch (e) {
-      next(e);
-    }
-});
-
-
-/*
 
 router.post("/signup", isLoggedOut(), async (req, res, next) => {
   const { username, password } = req.body;
-
   if (!username || !password) {
     req.flash("error", "Please, fill all the fields");
     return res.redirect("/signup");
   }
-
   try {
-    const existUser = await User.findOne({ username });
-    if (!existUser) {
-      const salt = bcrypt.genSaltSync(10);
-      const hashPass = bcrypt.hashSync(password, salt);
-      await User.create({ username, password: hashPass });
-      req.flash("success", "User created");
-      return res.redirect("/");
+    const existingUser = await User.findOne({
+      username
+    });
+    if (!existingUser && strength(password) >= 2) {
+      const newUser = await User.create({
+        username,
+        password: hashPassword(password)
+      });
+
+      // login with the user created
+      req.login(newUser, function(err) {
+        if (!err) {
+          return res.redirect("/");
+        } else {
+          req.flash("error", "No se ha podido acceder con el usuario creado");
+          return res.redirect("/");
+        }
+      });
+    } else if (strength(password) < 2) {
+      req.flash(
+        "error",
+        "Create a password with mixed case, special character and number (minimum 8 characters and no repeated letters)"
+      );
+      return res.redirect("/signup");
     } else {
-      req.flash("error", "User already exists");
+      req.flash("error", "The user or password already exists");
       return res.redirect("/signup");
     }
   } catch (e) {
     next(e);
   }
-});
-*/
-
-// Login
-router.get("/login", isLoggedOut(), (req, res, next) => {
-  return res.render("passport/login");
 });
 
 // Local strategy
